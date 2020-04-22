7d2e001bf5ad334ad730b28326a03eb1d076fd4c
dabaicai <316521750@qq.com>
2020-04-22 19:17:43 +0800

    线程按序交替执行案例：ABC

---
+++ src/com/zjj/juc02/TestABCAlternate.java
@@ -0,0 +1,135 @@
+package com.zjj.juc02;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/*
+编写一个程序，开启3个线程，这三个线程的 ID 分别为 A、B、C，每个线程将自己的ID 在屏幕上打印10遍，要求输出的结果必须按顺序显示
+    如：ABCABCABC....... 依次递归
+ */
+public class TestABCAlternate {
+    public static void main(String[] args) {
+        AlternateDemo ad = new AlternateDemo();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                for (int i = 1; i <= 20; i++) {
+                    ad.loopA(i);
+                }
+
+            }
+        },"A").start();
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                for (int i = 1; i <= 20; i++) {
+                    ad.loopB(i);
+                }
+
+            }
+        },"B").start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                for (int i = 1; i <= 20; i++) {
+                    ad.loopC(i);
+
+                    System.out.println("-----------");
+                }
+
+            }
+        },"C").start();
+    }
+}
+
+
+class AlternateDemo {
+
+    private int number = 1;
+    private Lock lock = new ReentrantLock();
+    private Condition condition1 = lock.newCondition();
+    private Condition condition2 = lock.newCondition();
+    private Condition condition3 = lock.newCondition();
+
+    public void loopA(int totalLoop) {
+        lock.lock();
+
+        try {
+            // 1. 判断
+            if (number != 1) {
+                condition1.await();
+            }
+
+            // 2. 打印
+            for (int i = 1; i <= 1; i++) {
+                System.out.println(Thread.currentThread().getName() + "\t" + i + "\t" + totalLoop);
+            }
+
+            // 3. 输出
+            number = 2;
+            condition2.signal();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void loopB(int totalLoop) {
+        lock.lock();
+
+        try {
+            // 1. 判断
+            if (number != 2) {
+                condition2.await();
+            }
+
+            // 2. 打印
+            for (int i = 1; i <= 1; i++) {
+                System.out.println(Thread.currentThread().getName() + "\t" + i + "\t" + totalLoop);
+            }
+
+            // 3. 输出
+            number = 3;
+            condition3.signal();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void loopC(int totalLoop) {
+        lock.lock();
+
+        try {
+            // 1. 判断
+            if (number != 3) {
+                condition3.await();
+            }
+
+            // 2. 打印
+            for (int i = 1; i <= 1; i++) {
+                System.out.println(Thread.currentThread().getName() + "\t" + i + "\t" + totalLoop);
+            }
+
+            // 3. 输出
+            number = 1;
+            condition1.signal();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+
+}
